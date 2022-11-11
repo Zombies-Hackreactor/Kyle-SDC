92bf7ddc817041e2b61c463e3ce1affa730e3913
Kyle Frandanisa <obliquetv@gmail.com>
2022-11-10 20:44:49 -0800

    loader endpoint

--- server/app.ts
+++ server/app.ts
@@ -8,6 +8,10 @@ app.use(express.json())
 
 app.use('/api', router)
 
+app.get('/loaderio-9c2280f546a969f1a0b4c8bcd188f1b6', function(req: any, res: any) {
+res.send('loaderio-9c2280f546a969f1a0b4c8bcd188f1b6')
+})
+
 // eslint-disable-next-line
 app.use((err: Error, req: Request, res: Response, _next: NextFunction) => {
   res.status(404).json({ error: 'Not found' })
